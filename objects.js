4960fb0f57c2f5609eba994d2e7ee849449efb8e
Santheep kumar <santheep.v@skematix.in>
2021-03-08 21:55:44 +0530

    Objects file added

---
+++ objects.js
@@ -0,0 +1,13 @@
+const obj = {
+  name: "santheep",
+  age: 22,
+  address: {
+    city: "Coimbatore",
+    state: "TN",
+  },
+};
+
+console.log(obj.name);
+console.log(obj.age);
+console.log(obj.address);
+console.log(obj.address);
