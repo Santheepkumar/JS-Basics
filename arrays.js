076ac36890769cea26fbc964ad9439fbe5bf0b41
Santheep kumar <santheep.v@skematix.in>
2021-03-08 22:24:47 +0530

    arrays files added

---
+++ arrays.js
@@ -0,0 +1,22 @@
+// const arr = [
+//   "santheep",
+//   {
+//     name: "santheep",
+//     age: 22,
+//     address: {
+//       city: "Coimbatore",
+//       state: "TN",
+//     },
+//   },
+//   "some",
+// ];
+
+// const first = arr[1];
+
+// console.log(first.name);
+
+let arr = ["santheep", "programming", 100, {}, []];
+
+arr.shift();
+
+console.log(arr);
